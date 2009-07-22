6602510795e155a9ab3af04d1dfb2444436c573e
lucaltb <lucaltb@0dc4f298-767e-11de-ac83-69fcb5c178b1>
2009-07-22 19:58:27 +0000

    implementacion con arreglo y main iniciado

---
+++ HojaTrabajo2/src/UsandoParqueo/placa.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package UsandoParqueo;
+
+/**
+ *
+ * @author Luis Carlos
+ */
+class placa {
+
+}
