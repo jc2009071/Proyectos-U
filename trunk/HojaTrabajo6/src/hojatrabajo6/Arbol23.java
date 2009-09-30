6f7020c45bc19f6014ccd07a155359ccd82474e3
lucaltb <lucaltb@0dc4f298-767e-11de-ac83-69fcb5c178b1>
2009-09-30 06:01:23 +0000

    menu iniciado

--- trunk/HojaTrabajo6/src/hojatrabajo6/Arbol23.java
+++ trunk/HojaTrabajo6/src/hojatrabajo6/Arbol23.java
@@ -9,6 +9,18 @@ package hojatrabajo6;
  *
  * @author Luis C
  */
-public class Arbol23 {
+public class Arbol23 implements Interfaz{
+
+    public void add(Object elemento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Object remove(Object elemento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String mostrar(String conjunto) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
---
+++ trunk/HojaTrabajo6/src/hojatrabajo6/Interfaz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hojatrabajo6;
+
+/**
+ *
+ * @author Luis C
+ */
+public interface Interfaz<E> {
+
+    public void add(E elemento);
+    public E remove(E elemento);
+    public String mostrar(String conjunto);
+}
