60ea16936ac73a6d373c9a3091c43b47b7205cb1
lucaltb <lucaltb@0dc4f298-767e-11de-ac83-69fcb5c178b1>
2009-09-30 06:01:23 +0000

    menu iniciado

--- HojaTrabajo6/src/hojatrabajo6/Main.java
+++ HojaTrabajo6/src/hojatrabajo6/Main.java
@@ -1,9 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package hojatrabajo6;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +14,122 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int opcion = 0;
+        String conjunto;
+        boolean error = true;
+        
+        do{
+
+        System.out.println("\n               MENU\n");
+        System.out.println("Eliga una opcion:");
+        System.out.println("1. Agregar Elementos al conjunto A o B");
+        System.out.println("2. Retirar Elementos del conjunto A o B");
+        System.out.println("3. Formar un conjunto nuevo C, que sea A union B");
+        System.out.println("4. Formar un conjunto nuevo C, que sea A interseccion B");
+        System.out.println("5. Mostrar cualquiera de los conjuntos");
+        System.out.println("6. Salir");
+
+            //try {
+                opcion = scan.nextInt();
+            /*} catch (InputMismatchException e) {
+            System.out.println("Error de ingreso.");
+            }*/
+
+        switch(opcion){
+            case 1:
+                error = true;
+                while(error){
+                    System.out.println("Elija conjunto: A o B");
+                    conjunto = scan.next();
+
+                    if(conjunto.equalsIgnoreCase("a")){
+                    error = false;
+
+                    }
+                    else
+                        if(conjunto.equalsIgnoreCase("b")){
+                        error = false;
+
+                        }
+                        else{
+                        System.out.println("No existe ese conjunto");
+                        error = true;
+                        }
+                }
+
+                break;
+
+            case 2:
+                error = true;
+                while(error){
+                    System.out.println("Elija conjunto: A o B");
+                    conjunto = scan.next();
+
+                    if(conjunto.equalsIgnoreCase("a")){
+                    error = false;
+
+                    }
+                    else
+                        if(conjunto.equalsIgnoreCase("b")){
+                        error = false;
+
+                        }
+                        else{
+                        System.out.println("No existe ese conjunto");
+                        error = true;
+                        }
+                }
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+                error = true;
+                while(error){
+                    System.out.println("Elija conjunto: A, B o C");
+                    conjunto = scan.next();
+
+                    if(conjunto.equalsIgnoreCase("a")){
+                    error = false;
+
+                    }
+                    else
+                        if(conjunto.equalsIgnoreCase("b")){
+                        error = false;
+
+                        }
+                        else
+                            if(conjunto.equalsIgnoreCase("c")){
+                            error = false;
+
+                            }
+                            else{
+                                System.out.println("No existe ese conjunto");
+                                error = true;
+                            }
+                }
+                break;
+
+            case 6:
+                System.out.println("Adios");
+                break;
+
+            default:
+                System.out.println("Error de ingreso.");
+                System.out.println();
+
+        }
+
+
+
+        }while(opcion!=6);
     }
 
 }
---
+++ HojaTrabajo6/src/hojatrabajo6/Nodo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hojatrabajo6;
+
+/**
+ *
+ * @author Luis C
+ */
+public class Nodo<E> {
+
+        private int data1, data2;
+        private Nodo<E> padre, izquierdo, derecho, centro;
+
+        
+}
