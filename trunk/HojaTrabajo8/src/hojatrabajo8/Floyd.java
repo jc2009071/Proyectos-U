d6d5e798a1b73446d3447d0c6c53f32667f6b952
lucaltb <lucaltb@0dc4f298-767e-11de-ac83-69fcb5c178b1>
2009-11-22 23:04:52 +0000

    crear proyecto hoja trabajo 8, clase grafo y main para probarla

---
+++ trunk/HojaTrabajo8/src/hojatrabajo8/Floyd.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hojatrabajo8;
+
+/**
+ *
+ * @author Luis C
+ */
+public class Floyd {
+
+
+
+
+}
---
+++ trunk/HojaTrabajo8/src/hojatrabajo8/Main.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hojatrabajo8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luis C
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner scan = new Scanner(System.in);
+        Grafo grafo = new Grafo();
+        int opcion,nodo1,nodo2;
+
+        do{
+            System.out.println();
+            System.out.println("menu");
+            System.out.println("1. agregar vertice");
+            System.out.println("2. agregar enlace");
+            System.out.println("3. encontrar camino mas corto");
+            System.out.println("4. salir");
+
+            opcion = scan.nextInt();
+
+            switch(opcion){
+                case 1:
+                    System.out.println("Ingrese nombre del vertice (numero): ");
+                    opcion=scan.nextInt();
+                    if(!grafo.agregarNodo(opcion))
+                        System.out.println("Ese vertice ya existe.");
+                    break;
+                case 2:
+                    System.out.println("Ingrese nombre del vertice fuente (numero): ");
+                    nodo1=scan.nextInt();
+                    System.out.println("Ingrese nombre del vertice destino (numero): ");
+                    nodo2=scan.nextInt();
+                    System.out.println("Ingrese peso del enlace (numero<1000): ");
+                    opcion=scan.nextInt();
+
+                    grafo.agregarEnlace(nodo1, nodo2, opcion);
+                    
+                    break;
+                case 3:
+                    grafo.toString();
+                    grafo.mostrar();
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    System.out.println("Ingreso invalido.");
+            }
+        }while (opcion!=4);
+
+
+    }
+
+}
