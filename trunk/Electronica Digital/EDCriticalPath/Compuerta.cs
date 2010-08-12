55fd94081f280f7266f9f6c7f12133c565bdfa1d
lucaltb <lucaltb@0dc4f298-767e-11de-ac83-69fcb5c178b1>
2010-08-12 19:38:10 +0000

    critical path project

---
+++ trunk/Electronica Digital/EDCriticalPath/Compuerta.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDCriticalPath
+{
+    class Compuerta
+    {
+
+        int id, valorP1, valorP2, delayP1R, delayP1F, delayP2R, delayP2F;
+        string nombre, conn;
+
+        public Compuerta() { }
+
+        public Compuerta(int ID, string Nombre, string DelayP1, string DelayP2, string Conn) {
+
+            id = ID;
+            valorP1 = -1;
+            valorP2 = -1;
+            nombre = Nombre;
+            string[] temp;
+
+            temp = DelayP1.Split('/');
+            if(temp.Length==2)
+                try {
+                    
+                    delayP1R = int.Parse(temp[0]);
+                    delayP1F = int.Parse(temp[1]);
+                }
+                catch (FormatException) {
+
+                    Console.WriteLine("Error al parsear delay P1 compuerta " + id);
+                }
+
+            temp = DelayP2.Split('/');
+            if (temp.Length == 2)
+                try {
+
+                    delayP2R = int.Parse(temp[0]);
+                    delayP2F = int.Parse(temp[1]);
+                }
+                catch (FormatException) {
+
+                    Console.WriteLine("Error al parsear delay P2 compuerta " + id);
+                }
+
+            conn = Conn;
+        }
+
+
+        public int getId() {
+
+            return id;
+        }
+
+        public int getDelayP1R() {
+
+            return delayP1R;
+        }
+
+        public int getDelayP1F() {
+
+            return delayP1F;
+        }
+
+        public int getDelayP2R() {
+
+            return delayP2R;
+        }
+
+        public int getDelayP2F() {
+
+            return delayP2F;
+        }
+
+        public int getValorP1() {
+
+            return valorP1;
+        }
+
+        public int getValorP2() {
+
+            return valorP2;
+        }
+
+        public void setValorP1(int valor) {
+
+            valorP1 = valor;
+        }
+
+        public void setValorP2(int valor) {
+
+            valorP2 = valor;
+        }
+
+        public void clearValores() {
+
+            valorP1 = -1;
+            valorP2 = -1;
+        }
+
+        public string getNombre() {
+
+            return nombre;
+        }
+
+        public string getConn() {
+
+            return conn;
+        }
+
+        public bool connect() {
+
+            //TODO llenar matriz de adyacencia compuertas
+
+            bool problem = false;
+            int posEntradas = Program.entradas.Count - 1;
+            string[] temp = conn.Split('|');
+            string[] array;
+
+            foreach (string temp1 in temp)
+            {
+
+                array = temp1.Split(',');
+
+                try {
+
+                    Program.matriz[int.Parse(array[0]) - 1][posEntradas + id - 1] = int.Parse(array[1]);
+                }
+                catch (FormatException) {
+
+                    problem = true;
+                }
+                catch (IndexOutOfRangeException) {
+
+                    problem = true;
+                }
+
+            }
+
+            return problem;
+        }
+
+    }
+}
---
+++ trunk/Electronica Digital/EDCriticalPath/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace EDCriticalPath
+{
+    class Program
+    {
+        public static List<Compuerta> compuertas = new List<Compuerta>();
+        public static List<Entrada> entradas = new List<Entrada>();
+        public static int compuertaFinal;
+        public static int[][] matriz;
+        
+        static void Main(string[] args) {
+
+            configs();
+            initializeM();
+        }
+
+        public static void initializeM() { 
+        
+            matriz = new int[compuertas.Count][];
+
+            for (int i = 0; i < matriz.Length; i++)
+                matriz[i] = new int[(compuertas.Count + entradas.Count)];
+
+            //TODO llamar a metodos en compuertas y entradas para llenar
+        }
+
+        public static void showMatriz() {
+            
+            //TODO deplegar matriz
+            foreach (int[] a in matriz) 
+                foreach (int i in a) {
+
+                    Console.Write("");
+                }
+        }
+
+
+        public static void configs() {
+
+            XmlTextReader reader = new XmlTextReader(@"C:\Users\Public\ED\circuito1.xml");
+
+            while (reader.Read()) {
+
+                switch (reader.NodeType) {
+
+                    case XmlNodeType.Element:
+
+                        switch (reader.Name) {
+
+                            case "Compuertas":
+
+                                compuertaFinal = int.Parse(reader.GetAttribute("CompuertaFinal"));
+                                break;
+
+                            case "compuerta":
+
+                                int id = int.Parse(reader.GetAttribute("id"));
+                                string name, delayP1, delayP2, conn;
+                                name = reader.GetAttribute("name");
+                                delayP1 = reader.GetAttribute("delayP1");
+                                delayP2 = reader.GetAttribute("delayP2");
+                                conn = reader.GetAttribute("conn");
+                                Compuerta temp = new Compuerta(id, name, delayP1, delayP2, conn);
+                                compuertas.Add(temp);
+                                break;
+
+                            case "entrada":
+
+                                int id1 = int.Parse(reader.GetAttribute("id"));
+                                string nombre1, conn1;
+                                nombre1 = reader.GetAttribute("name");
+                                conn1 = reader.GetAttribute("conn");
+                                Entrada temp1 = new Entrada(id1, nombre1, conn1);
+                                entradas.Add(temp1);
+                                break;
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
